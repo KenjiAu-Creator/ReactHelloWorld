06978895962bb08020adbb34f30b3b2929fea759
Kenji <kenji.au9@gmail.com>
2020-09-11 09:55:40 -0600

    Added in initial HelloStudents component

---
+++ src/components/HelloStudents.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import HelloStudent from './HelloStudent';
+
+function HelloStudents()
+{
+  // Some test values.
+  const students = [ "Lindsey", "Ayesha", "Krisztina" ];
+  
+  //Create a map. (versus an array.)
+  const studentsList = students.map( 
+    studentName => <HelloStudent name={studentName} />
+  );
+  
+  console.log(studentsList);
+  
+  return (
+    <section>
+      <h2>Our Hello to Students</h2>
+        { studentsList }
+    </section>
+  );
+}
+
+export default HelloStudents;
